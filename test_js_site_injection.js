4e748f3419f1e7ab87c7048388139326fbce0ac1
Saurabh Ghanekar <ghanekarsaurabh8@gmail.com>
2024-10-21 08:11:50 -0700

    Add an MutationObserver in test_js_site_injection.js

--- test_js_site_injection.js
+++ test_js_site_injection.js
@@ -1,1 +1,16 @@
-console.log("Testing Biodex js site injection")
+let observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    let oldValue = mutation.oldValue;
+    let newValue = mutation.target.textContent;
+    if (oldValue !== newValue) {
+        console.log("Testing Biodex js site injection")
+    }
+  });
+});
+
+observer.observe(document.body, {
+  characterDataOldValue: true, 
+  subtree: true, 
+  childList: true, 
+  characterData: true
+});
