671a7edd4cf9c411926ca67363026bb4097bb1ed
Saurabh Ghanekar <ghanekarsaurabh8@gmail.com>
2024-11-11 15:24:15 -0800

    Update test_js_site_injection.js

--- test_js_site_injection.js
+++ test_js_site_injection.js
@@ -5,55 +5,6 @@
     injectContentScript();
     window.isContentScriptInjected = true;
 
-    // Override pushState and replaceState
-    const originalPushState = history.pushState;
-    const originalReplaceState = history.replaceState;
-
-    history.pushState = function (...args) {
-      originalPushState.apply(this, args);
-      window.dispatchEvent(new Event("spa-navigation"));
-    };
-
-    history.replaceState = function (...args) {
-      originalReplaceState.apply(this, args);
-      window.dispatchEvent(new Event("spa-navigation"));
-    };
-
-    // Listen for SPA navigation (popstate and custom events)
-    window.addEventListener("popstate", () => {
-      window.dispatchEvent(new Event("spa-navigation"));
-    });
-
-    window.addEventListener("spa-navigation", onSPAnavigation);
-
-    // Capture click events on the page
-    let lastClickEvent = null;
-    document.addEventListener("click", (event) => {
-      lastClickEvent = event;
-    });
-
-    function onSPAnavigation() {
-      // Re-inject or notify content script
-      if (!window.isContentScriptInjected) {
-        injectContentScript();
-        window.isContentScriptInjected = true;
-      } else {
-        if (lastClickEvent) {
-          window.postMessage(
-            {
-              message: "pageUpdated",
-              clickEvent: lastClickEvent,
-            },
-            "*"
-          );
-          lastClickEvent = null;
-        } else {
-          window.postMessage({ message: "pageUpdated" }, "*");
-        }
-      }
-    }
-  }
-
   function injectContentScript() {
     // Create a new <script> element and inject it
     const script = document.createElement("script");
